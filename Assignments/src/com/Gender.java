fde09724f23f4d85cab8e6e9ad4e53e23fc1cefa
PrasunPK <prasun_1993@rediffmail.com>
2016-03-03 17:55:53 +0530

    Few methods are shifted to Name.

---
+++ Assignments/src/com/Gender.java
@@ -0,0 +1,17 @@
+package com;
+
+public enum Gender {
+    MALE("Mr"),
+    FEMALE("Ms");
+
+    private String prefix;
+
+    Gender(String prefix) {
+        this.prefix  = prefix;
+    }
+
+    @Override
+    public String toString() {
+        return prefix;
+    }
+}
